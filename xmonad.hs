381adbbae5a9c1912e79f68f98eebad4f65c7ab5
cornet <cornet@sheepy.org>
2010-01-13 22:33:02 +0000

    - Added grid layout - Floating for GIMP

--- xmonad.hs
+++ xmonad.hs
@@ -1,6 +1,8 @@
 import XMonad
 import XMonad.Config.Gnome
 import XMonad.ManageHook
+import XMonad.Config.Desktop
+import XMonad.Layout.Grid
 import qualified Data.Map as M
 
 myManageHook :: [ManageHook]
@@ -9,9 +11,24 @@ myManageHook =
     , resource =? "Do.exe" --> doIgnore
     , resource =? "/usr/lib/gnome-do/Do.exe" --> doIgnore
     , resource =? "gnome-do" --> doIgnore
-    , className =? "Gimp" --> doFloat ]
+    , className =? "Gimp-2.6" --> doFloat ]
+
+myLayoutHook = desktopLayoutModifiers $ tiled ||| Mirror tiled ||| Full ||| Grid
+  where
+     -- default tiling algorithm partitions the screen into two panes
+     tiled   = Tall nmaster delta ratio
+ 
+     -- The default number of windows in the master pane
+     nmaster = 1
+ 
+     -- Default proportion of screen occupied by master pane
+     ratio   = 1/2
+ 
+     -- Percent of screen to increment by when resizing panes
+     delta   = 3/100
 
 main = xmonad gnomeConfig
     { modMask = mod4Mask 
     , manageHook = manageHook gnomeConfig <+> composeAll myManageHook
+    , layoutHook = myLayoutHook
     }
