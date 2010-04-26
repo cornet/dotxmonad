f1ad869b819cb4361aab6b599982d13076f618d4
cornet <cornet@sheepy.org>
2010-04-26 20:47:51 +0100

    - Using xmonad-log-applet instead of xmobar - Added Shell prompt - Make use of Actions.WindowGo

--- xmonad.hs
+++ xmonad.hs
@@ -3,16 +3,54 @@ import XMonad.Config.Gnome
 import XMonad.ManageHook
 import XMonad.Config.Desktop
 import XMonad.Layout.Grid
+import XMonad.Hooks.DynamicLog
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Actions.WindowGo
+import XMonad.Prompt
+import XMonad.Prompt.Shell
+import XMonad.Prompt.XMonad
+import System.IO
+import DBus
+import DBus.Connection
+import DBus.Message
+import Control.OldException
+import Control.Monad
+
 import qualified Data.Map as M
 
-myManageHook :: [ManageHook]
-myManageHook =
-    [ resource =? "Do" --> doIgnore
-    , resource =? "Do.exe" --> doIgnore
-    , resource =? "/usr/lib/gnome-do/Do.exe" --> doIgnore
-    , resource =? "gnome-do" --> doIgnore
-    , className =? "Gimp-2.6" --> doFloat ]
+-- This retry is really awkward, but sometimes DBus won't let us get our
+-- name unless we retry a couple times.
+getWellKnownName :: Connection -> IO ()
+getWellKnownName dbus = tryGetName `catchDyn` (\ (DBus.Error _ _) ->
+                                                getWellKnownName dbus)
+ where
+  tryGetName = do
+    namereq <- newMethodCall serviceDBus pathDBus interfaceDBus "RequestName"
+    addArgs namereq [String "org.xmonad.Log", Word32 5]
+    sendWithReplyAndBlock dbus namereq 0
+    return ()
 
+--
+-- My Manage Hook
+--
+myManageHook :: [ManageHook]
+myManageHook = 
+    [ resource  =? "Do"                       --> doIgnore
+    , resource  =? "Do.exe"                   --> doIgnore
+    , resource  =? "/usr/lib/gnome-do/Do.exe" --> doIgnore
+    , resource  =? "gnome-do"                 --> doIgnore
+    , resource  =? "Dialog"                   --> doFloat
+    , title     =? "Firefox Preferences"      --> doFloat
+    , title     =? "Namoroka Preferences"     --> doFloat
+    , title     =? "Thunderbird Preferences"  --> doFloat
+    , title     =? "Shredder Preferences"     --> doFloat
+    , className =? "Gimp-2.6"                 --> doFloat
+    ]
+
+
+--
+-- My Layout Hook
+--
 myLayoutHook = desktopLayoutModifiers $ tiled ||| Mirror tiled ||| Full ||| Grid
   where
      -- default tiling algorithm partitions the screen into two panes
@@ -27,8 +65,64 @@ myLayoutHook = desktopLayoutModifiers $ tiled ||| Mirror tiled ||| Full ||| Grid
      -- Percent of screen to increment by when resizing panes
      delta   = 3/100
 
-main = xmonad gnomeConfig
-    { modMask = mod4Mask 
-    , manageHook = manageHook gnomeConfig <+> composeAll myManageHook
-    , layoutHook = myLayoutHook
-    }
+--
+-- My Keybindings
+--
+myKeys conf@(XConfig {XMonad.modMask = modm}) =
+    [ ((modm, xK_F2 ), shellPrompt  defaultXPConfig)
+    , ((modm, xK_f), runOrRaise "firefox" (className =? "Namoroka"))
+    , ((modm, xK_s), runOrRaise "thunderbird" (className =? "Shredder"))
+    ]
+
+-- Merge myKeys with default config
+newKeys x  = M.union (keys gnomeConfig x) (M.fromList (myKeys x))
+
+--
+-- Main Configuration
+--
+main :: IO ()
+main = withConnection Session $ \ dbus -> do
+     putStrLn "Getting well-known name."
+     getWellKnownName dbus
+     putStrLn "Got name, starting XMonad."
+     xmonad $ gnomeConfig
+	    { modMask = mod4Mask 
+            , keys = newKeys
+	    , manageHook = manageHook gnomeConfig <+> composeAll myManageHook
+	    , layoutHook = myLayoutHook
+            , logHook    = dynamicLogWithPP $ defaultPP {
+                             ppOutput   = \ str -> do
+                               let str'  = "<span font=\"Monospace 9\">" ++ str ++ 
+                                           "</span>"
+                                   str'' = sanitize str'
+                               msg <- newSignal "/org/xmonad/Log" "org.xmonad.Log" 
+                                          "Update"
+                               addArgs msg [String str'']
+                               -- If the send fails, ignore it.
+                               send dbus msg 0 `catchDyn`
+                                 (\ (DBus.Error _name _msg) ->
+                                   return 0)
+                               return ()
+                  , ppTitle    = pangoColor "#ffffff" . shorten 50
+                  , ppCurrent  = pangoColor "#ffffff" . wrap "[" "]"
+                  , ppVisible  = pangoColor "#663366" . wrap "(" ")"
+                  , ppHidden   = wrap "" ""
+                  , ppUrgent   = pangoColor "red"
+	    }
+      }
+
+--
+-- Colouring and saitizing for xmonad-log-applet
+--
+pangoColor :: String -> String -> String
+pangoColor fg = wrap left right
+ where
+  left  = "<span foreground=\"" ++ fg ++ "\">"
+  right = "</span>"
+
+sanitize :: String -> String
+sanitize [] = []
+sanitize (x:rest) | fromEnum x > 127 = "&#" ++ show (fromEnum x) ++ "; " ++
+                                       sanitize rest
+                  | otherwise        = x : sanitize rest
+
